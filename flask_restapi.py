c0e0bc575ec47191d079b31e54271f1e0dc137f9
Alberto Benayas <benayas1@gmail.com>
2020-11-08 23:45:53 +0000

    e

--- flask_restapi.py
+++ flask_restapi.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 import pickle
 import numpy as np
-from model import NLPModel
+from src/models import NLPModel
 from settings import *
 
 app = Flask(__name__)
